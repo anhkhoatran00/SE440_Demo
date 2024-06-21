b5e97aadb434ddb5e498a9e5a1b7d959069bbe0c
Tuleeeeeeee <tule1772003@gmail.com>
2024-06-21 07:20:40 +0700

    Add water shader and make object floating

---
+++ Physic/Assets/Scripts/FloatingObject.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class FloatingObject : MonoBehaviour
+{
+    [SerializeField]
+    private float underWaterDrag;
+    [SerializeField]
+    private float underWaterAngularDrag;
+    [SerializeField]
+    private float airDrag;
+    [SerializeField]
+    private float airAngularDrag;
+    [SerializeField]
+    private float waterPower;
+    [SerializeField]
+    private Transform[] floatPoints;
+
+    private Rigidbody rb;
+    private bool isUnderWater;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        int pointUnderWaterCount = 0;
+        foreach (var point in floatPoints)
+        {
+            var diff = point.position.y - 0;
+            if (diff < 0)
+            {
+                rb.AddForceAtPosition(Vector3.up * waterPower * Mathf.Abs(diff), point.position, ForceMode.Acceleration);
+                pointUnderWaterCount++;
+                if (!isUnderWater)
+                {
+                    isUnderWater = true;
+                    SetStage(true);
+                }
+            }
+        }
+
+
+        if (isUnderWater && pointUnderWaterCount == 0)
+        {
+            isUnderWater = false;
+            SetStage(false);
+        }
+    }
+    private void SetStage(bool underWater)
+    {
+        if (underWater)
+        {
+            rb.drag = underWaterDrag;
+            rb.angularDrag = underWaterAngularDrag;
+        }
+        else
+        {
+            rb.drag = airDrag;
+            rb.angularDrag = airAngularDrag;
+        }
+    }
+
+}
